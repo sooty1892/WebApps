4d683b94e42db675f6b0500f6c08f1949cf991e5
Edwin K <edwink@ubuntu.(none)>
2014-05-27 18:02:16 +0100

    Added revised homepage and javascript that include jQuery animations. Planning to add another section for the homepage that regards connecting to social media.

---
+++ js/sliding.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+            $("#jumbo").animate({left : "+=1300"}, 1450, "easeOutBounce");
+            $("#about").animate({right : "+=1300"}, 1450, "easeOutBounce"); 
+});
