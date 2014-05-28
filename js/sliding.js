a6eddb2f920e479a0bc33c187012fc98f18d344e
Edwin K <edwink@ubuntu.(none)>
2014-05-28 16:47:36 +0100

    Added error messaging to sign up and added some extra js for displaying which type of error occurs.

--- js/sliding.js
+++ js/sliding.js
@@ -1,4 +1,37 @@
 $(document).ready(function() {
-            $("#jumbo").animate({left : "+=1300"}, 1450, "easeOutBounce");
-            $("#about").animate({right : "+=1300"}, 1450, "easeOutBounce"); 
+    $("#jumbo").animate({left : "+=1300"}, 1450, "easeOutBounce");
+    $("#about").animate({right : "+=1300"}, 1450, "easeOutBounce"); 
+    
+    $("#btnLogin").click(function () {
+        /* Will need to add some validtion here and then make use of
+           of the the slideDown animation */
+        /*var usr = document.getElementById("username");
+        var passwrd = document.getElementById("password");
+        
+        if(usr.value != passwrd.value) {
+            $("#login-error").slideDown("slow");   
+        } else {
+            $("#login-error").slideUp("slow");
+        }*/
+
+        $("#login-error").slideDown("slow"); 
+    });
+    
+    $("#btnSignUp").click(function () {
+        var userLogin = document.getElementById("user-login");
+        var userEmail = document.getElementById("user-email");
+        var password = document.getElementById("user-password");
+        var passwordConfirm = document.getElementById("user-password-confirm");
+        
+        if (password.value != passwordConfirm.value) {
+            document.getElementById("password-match").innerHTML = "Passwords do no match";
+            $("#password-match").slideDown("slow");
+        } else if (password.value == "" && passwordConfirm.value == "") {
+            document.getElementById("password-match").innerHTML = "Please enter a suitable password";
+            $("#password-match").slideDown("slow");
+        } else {
+            $("#password-match").slideUp("slow");
+        }
+    });
+        
 });
