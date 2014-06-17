697dd3d6656d23f233f502014a635c212b003bb0
sooty1892 <ashemingway.uni@gmail.com>
2014-06-17 23:18:17 +0100

    chat stuff

--- chat.php
+++ chat.php
@@ -19,7 +19,31 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+
+ <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Music Collaborator</title>
+    <link rel="stylesheet" href="css/jquery.simplecolorpicker.css">
+    <link href="css/player.css" rel="stylesheet">
+    <link href="css/jquery.simplecolorpicker.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/lightbox.css" rel="stylesheet">
+    <link href="css/project_styles.css" rel="stylesheet">
     <link href="css/chat.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script> 
+    <script src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/jquery.jplayer.min.js"></script>
+    <script type="text/javascript" src="js/jplayer.playlist.min.js"></script>  
+    <script type="text/javascript" src="js/jquery.pagescroller.lite.js"></script>
+    <script src="js/create_project_add.js"></script>
+    <script src="js/edit_project_add.js"></script>
+
+    <script src="js/jquery.simplecolorpicker.js"></script>
+    <script src="js/project.js"></script>
+    <script type="text/javascript"> 
+
   	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
   	<script>
   		var last = 0;
@@ -63,7 +87,7 @@
 				success: function(response) {
 					console.log(response);
 					for (var i in response) {
-						$('#chat-area').append($("</p>" +"<span>" + response[i].username + "</span>" + ":\n" + response[i].message + "</p>"));
+						$('#chat-area').append($("<p>" +"<span>" + response[i].username + "</span>" + "\n" + response[i].message + "</p>"));
 						if (response[i].idmessage > last) {
 							last = response[i].idmessage;
 						}
@@ -77,7 +101,6 @@
   <body>
   	<div id="page-wrap">
     
-        <h2>LIMITED TO 100 CHARACTERS ATM</h2>
         
         <div id="chat-wrap">
         	<div id="chat-area">
